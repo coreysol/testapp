89aedcad9c76bbc9abd11ab6ea53361860b46b13
Corey Ramsden <corey.ramsden@gmail.com>
2014-08-20 13:38:03 -0400

    Update, edit changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,43 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 
   def create
-    render plain: params[:article].inspect
+#   This is a test below that displays the form values hash to the browser
+#   render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
